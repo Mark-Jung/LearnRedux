c47c78816b06009aec999adf73cefa28ef7091bd
Mark Jung <gujung2022@u.northwestern.edu>
2017-07-03 12:13:29 +0900

    learned basics of store

---
+++ app/redux-example.jsx
@@ -0,0 +1,29 @@
+var redux = require('redux');
+
+console.log('Starting redux example');
+
+var reducer = (state = {name: 'Anonymous'}, action) => {
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      };
+    default:
+      return state;
+  }
+
+};
+var store = redux.createStore(reducer);
+
+var currentState = store.getState();
+console.log('currentState', currentState);
+
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Mark'
+});
+
+
+
+console.log('Name should be Mark', store.getState());
