6fbb15808228cdc234c6430e6a67815a86cf9613
Mark Jung <gujung2022@u.northwestern.edu>
2017-07-04 01:03:15 +0900

    learned removing state

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,12 +2,53 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-var reducer = (state = {name: 'Anonymous'}, action) => {
+var stateDefault ={
+  name: 'Anonymous',
+  hobbies: [],
+  movies: []
+};
+
+var nextHobbyId = 1;
+var nextMovieId = 1;
+var reducer = (state = stateDefault, action) => {
   switch (action.type) {
     case 'CHANGE_NAME':
       return {
         ...state,
         name: action.name
+      };
+    case 'ADD_HOBBY':
+      return {
+        ...state,
+        hobbies: [
+          ...state.hobbies,
+          {
+            id: nextHobbyId++,
+            hobby: action.hobby
+          }
+        ]
+      };
+    case 'ADD_MOVIE':
+      return {
+        ...state,
+        movies: [
+          ...state.movies,
+          {
+            id: nextMovieId++,
+            name: action.movie.name,
+            genre: action.movie.genre
+          }
+        ]
+      };
+    case 'REMOVE_HOBBY':
+      return {
+        ...state,
+        hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
+      };
+    case 'REMOVE_MOVIE':
+      return {
+        ...state,
+        movies: state.movies.filter((movie) => movie.id !== action.id)
       };
     default:
       return state;
@@ -23,6 +64,7 @@ var unsubscribe = store.subscribe( () => {
 
   console.log('Name is', state.name);
   document.getElementById('app').innerHTML = state.name;
+  console.log('New state', state.hobbies);
 });
 
 var currentState = store.getState();
@@ -33,9 +75,37 @@ store.dispatch({
   name: 'Mark'
 });
 
-// unsubscribe();
-
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Running'
+});
 store.dispatch({
   type: 'CHANGE_NAME',
   name: 'Emily'
+});
+store.dispatch({
+  type: 'ADD_MOVIE',
+  movie: {
+    name: 'Three Idiots',
+    genre: 'comedy'
+  }
+});
+store.dispatch({
+  type: 'ADD_MOVIE',
+  movie: {
+    name: 'Avengers',
+    genre: 'action'
+  }
+});
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'ball'
+});
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+});
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 2
 });
